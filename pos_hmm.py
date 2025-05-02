856225fcdcf929dff8affdd4e2b0f508637a9f6a
Matthew Valencia <120185909+HirokiShinsei@users.noreply.github.com>
2025-05-02 22:42:23 +0800

    Updated some files for correction

--- pos_hmm.py
+++ pos_hmm.py
@@ -1,4 +1,3 @@
-import numpy as np
 from collections import defaultdict
 
 class HMM:
@@ -10,13 +9,12 @@ class HMM:
         self.emit_probs = defaultdict(lambda: defaultdict(float))
 
     def train(self, tagged_sentences):
-        tag_counts = defaultdict(int)
+        start_counts = defaultdict(int)
         transition_counts = defaultdict(lambda: defaultdict(int))
         emission_counts = defaultdict(lambda: defaultdict(int))
-        start_counts = defaultdict(int)
+        tag_counts = defaultdict(int)
 
         for sentence in tagged_sentences:
-            prev_tag = None
             for i, (word, tag) in enumerate(sentence):
                 self.states.add(tag)
                 self.vocab.add(word)
@@ -25,44 +23,44 @@ class HMM:
 
                 if i == 0:
                     start_counts[tag] += 1
-                if prev_tag is not None:
+                else:
+                    prev_tag = sentence[i-1][1]
                     transition_counts[prev_tag][tag] += 1
-                prev_tag = tag
 
         total_starts = sum(start_counts.values())
+        # Compute probabilities with optional smoothing
+        smoothing = 1e-6
         for tag in self.states:
-            self.start_probs[tag] = start_counts[tag] / total_starts
+            self.start_probs[tag] = (start_counts[tag] + smoothing) / (total_starts + smoothing * len(self.states))
             for next_tag in self.states:
-                self.trans_probs[tag][next_tag] = (
-                    transition_counts[tag][next_tag] / tag_counts[tag]
-                )
+                self.trans_probs[tag][next_tag] = (transition_counts[tag][next_tag] + smoothing) / (tag_counts[tag] + smoothing * len(self.states))
             for word in self.vocab:
-                self.emit_probs[tag][word] = (
-                    emission_counts[tag][word] / tag_counts[tag]
-                )
+                self.emit_probs[tag][word] = (emission_counts[tag][word] + smoothing) / (tag_counts[tag] + smoothing * len(self.vocab))
 
     def viterbi(self, sentence):
         V = [{}]
         path = {}
 
         for tag in self.states:
-            V[0][tag] = self.start_probs[tag] * self.emit_probs[tag].get(sentence[0], 1e-6)
+            V[0][tag] = self.start_probs.get(tag, 1e-6) * self.emit_probs[tag].get(sentence[0], 1e-6)
             path[tag] = [tag]
 
         for t in range(1, len(sentence)):
             V.append({})
             new_path = {}
-
-            for curr_tag in self.states:
-                max_prob, best_prev_tag = max(
-                    (V[t - 1][prev_tag] * self.trans_probs[prev_tag].get(curr_tag, 1e-6) *
-                     self.emit_probs[curr_tag].get(sentence[t], 1e-6), prev_tag)
+            for curr in self.states:
+                (prob, prev_state) = max(
+                    (
+                        V[t - 1][prev_tag] *
+                        self.trans_probs[prev_tag].get(curr, 1e-6) *
+                        self.emit_probs[curr].get(sentence[t], 1e-6),
+                        prev_tag
+                    )
                     for prev_tag in self.states
                 )
-                V[t][curr_tag] = max_prob
-                new_path[curr_tag] = path[best_prev_tag] + [curr_tag]
-
+                V[t][curr] = prob
+                new_path[curr] = path[prev_state] + [curr]
             path = new_path
 
-        max_final_prob, best_final_tag = max((V[-1][tag], tag) for tag in self.states)
-        return path[best_final_tag]
+        final_state = max(self.states, key=lambda tag: V[-1].get(tag, 0))
+        return path[final_state]
--- valenca_6_1.py
+++ valenca_6_1.py
@@ -1,20 +1,35 @@
+import string
 from pos_hmm import HMM
 
-# dataset
+def preprocess_sentence(sentence, vocab):
+    # normalization
+    tokens = [word.lower().strip(string.punctuation) for word in sentence.split()]
+    return [token if token in vocab else '<UNK>' for token in tokens]
+
+# dataset 
 tagged_sentences = [
-    [("The", "DET"), ("cat", "NOUN"), ("sleeps", "VERB")],
-    [("A", "DET"), ("dog", "NOUN"), ("barks", "VERB")],
-    [("The", "DET"), ("dog", "NOUN"), ("sleeps", "VERB")],
-    [("My", "DET"), ("dog", "NOUN"), ("runs", "VERB"), ("fast", "ADV")],
-    [("A", "DET"), ("cat", "NOUN"), ("meows", "VERB"), ("loudly", "ADV")],
-    [("Your", "DET"), ("cat", "NOUN"), ("runs", "VERB")],
-    [("The", "DET"), ("bird", "NOUN"), ("sings", "VERB"), ("sweetly", "ADV")],
-    [("A", "DET"), ("bird", "NOUN"), ("chirps", "VERB")]
+    [(word.lower(), tag) for word, tag in sent] for sent in [
+        [("The", "DET"), ("cat", "NOUN"), ("sleeps", "VERB")],
+        [("A", "DET"), ("dog", "NOUN"), ("barks", "VERB")],
+        [("The", "DET"), ("dog", "NOUN"), ("sleeps", "VERB")],
+        [("My", "DET"), ("dog", "NOUN"), ("runs", "VERB"), ("fast", "ADV")],
+        [("A", "DET"), ("cat", "NOUN"), ("meows", "VERB"), ("loudly", "ADV")],
+        [("Your", "DET"), ("cat", "NOUN"), ("runs", "VERB")],
+        [("The", "DET"), ("bird", "NOUN"), ("sings", "VERB"), ("sweetly", "ADV")],
+        [("A", "DET"), ("bird", "NOUN"), ("chirps", "VERB")]
+    ]
 ]
 
 # HMM model and training
 model = HMM()
 model.train(tagged_sentences)
+
+# Build a vocabulary including an unknown token
+vocab = set()
+for sent in tagged_sentences:
+    for word, _ in sent:
+        vocab.add(word)
+vocab.add('<UNK>')
 
 # Interactive testing loop
 while True:
@@ -23,8 +38,8 @@ while True:
         print("Exiting...")
         break
 
-    test_sentence = user_input.split()
+    test_sentence = preprocess_sentence(user_input, vocab)
     predicted_tags = model.viterbi(test_sentence)
 
-    print("Sentence:", test_sentence)
+    print("Preprocessed Sentence:", test_sentence)
     print("Predicted Tags:", predicted_tags)
